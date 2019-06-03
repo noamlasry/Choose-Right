259e54258daefc8546303a652933f0bc3ef2c008
Yitzchak Schwarz <s.y.schwarz@gmail.com>
2019-06-03 17:51:51 +0300

    Profile editing now previews before committing

--- Ladaat/src/app/profile/profile.component.ts
+++ Ladaat/src/app/profile/profile.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService, User } from '../login/user.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -8,32 +8,37 @@ import { UserService, User } from '../login/user.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-	user: User;
-	file: File;
+	user: User = new User();
+  file: File;
+  @ViewChild('picture') img;
 
   constructor(
-		private userAuth: AngularFireAuth,
-		private userService: UserService
+    private userService: UserService,
+    private location: Location
   ) { }
 
   ngOnInit() {
 		this.userService.onChange(user => {
       if (user) {
-				this.user = user;
-      }
-      else {
-        this.user = new User();
+        this.user.avatar = user.avatar;
+        this.user.email = user.email;
+        this.user.name = user.name;
       }
 		});
   }
+
 	onAvatarChanged(event) {
 		if(event.target.files.length > 0) {
-			this.file = event.target.files[0];
-			this.userService.setAvatar(this.file);
-		}
-	}
+      this.file = event.target.files[0];
+      this.img.nativeElement.src = URL.createObjectURL(this.file);
+    }
+  }
+  
+  onSave() {
+    if (this.file) {
+      this.userService.setAvatar(this.file);
+    }
 
-  onNickChanged() {
-		this.userService.setName(this.user.name);
+    this.userService.setName(this.user.name);
   }
 }
